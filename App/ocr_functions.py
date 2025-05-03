7995e26cd40d1ad2556c833428b754f46a8d17dc
Sofia Caruso <sofia.caruso04k@gmail.com>
2025-05-03 12:03:17 +0200

    Implementata funzione di correzione del testo - Funzione che utilizza la libreria spellchecker per  fare un controllo ortografico, parola per parola, del testo estratto con l'OCR

--- App/ocr_functions.py
+++ App/ocr_functions.py
@@ -5,6 +5,7 @@ import time
 import os
 import cv2
 import numpy as np
+from spellchecker import SpellChecker
 
 IMAGE_DIR = "Images/"  # Cartella delle immagini
 
@@ -40,18 +41,48 @@ def analyze_image(file_path):
     return binary_img
 
 
+def correct_text(text):
+    """
+    Funzione per correggere il testo estratto tramite OCR utilizzando la libreria pyspellchecker
+    - Controlla il testo (permette di selezionare la lingua => tra cui it, en)
+    - Divide il testo, ogni volta che incontra uno spazio, in una lista di parole
+    - Corregge, se c'è bisogno, le parole e le aggiugne alla lista delle parole corrette
+    - Ricostruisce il testo corregendo gli errori
+    :param text: testo estratto dall'immagine
+    :return: testo corretto
+    """
+    spell = SpellChecker(language='it')
+    words = text.split()
+    corrected_words = []
+
+    for word in words:
+        if word:
+            corrected_word = spell.correction(word)
+            if corrected_word is None:
+                corrected_word = word
+            corrected_words.append(corrected_word)
+        else:
+            corrected_words.append(word)
+
+    corrected_text = " ".join(corrected_words)
+
+    return corrected_text
+
+
 def extract_text_from_image(data):
     """
     Funzione per estrarre il testo da un'immagine attraverso l'OCR
     - Recupera i dati presenti nel database (in caso contrario, stampa un messaggio)
     - Seleziona il file da poter processare tra quelli presenti nel database ed estrae il percorso dell'immagine
     - Permette di selezionare se applicare il pre-processing oppure no
-    - Crea un bottone per eseguire l'OCR sul file utilizzando la libreria (permette di selezionare la lingua)
+    - Crea un bottone per eseguire l'OCR sul file utilizzando la libreria pytesseract
+      (permette di selezionare la lingua => tra cui ita, eng)
     - Crea due colonne per visualizzare l'immagine e il relativo testo
     :param data: dati presenti nel database
     :return: testo estratto dall'immagine
     """
-    extracted_text = ""
+    #extracted_text = ""
+    corrected_text = ""
 
     if data:
         file_to_process = st.selectbox("Select file to process with OCR", [row[1] for row in data])
@@ -73,16 +104,21 @@ def extract_text_from_image(data):
             else:
                 img = img_original
 
-            extracted_text = pytesseract.image_to_string(img, lang="eng")
+            config = r'--oem 3 --psm 4'
+            extracted_text = pytesseract.image_to_string(img, config=config, lang="eng")
+            corrected_text = correct_text(extracted_text)
 
-            col1, col2 = st.columns([1, 1])
+            col1, col2, col3 = st.columns([1, 1, 1])
             with col1:
                 st.image(img, caption=f"Selected Image: {file_to_process}", use_container_width=True)
             with col2:
-                st.write(f"Extracted text from {file_to_process}:")
+                st.write(f"Extracted text (raw OCR) from {file_to_process}:")
                 st.text(extracted_text)
+            with col3:
+                st.write(f"Corrected text from {file_to_process}:")
+                st.text(corrected_text)
 
     else:
         st.info("No data available in the database for processing.")
 
-    return extracted_text
+    return corrected_text #extracted_text
