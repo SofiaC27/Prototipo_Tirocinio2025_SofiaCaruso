f28c5189476e96d090d4f162c7b57c1b2b3ce8e5
Sofia Caruso <sofia.caruso04k@gmail.com>
2025-07-12 17:55:53 +0200

    Integrazione Chainlit-LangChain basata sul tutorial della documentazione ufficiale  -Configurato LLM Groq (llama3) con chiave API via Streamlit

--- chainlit_app.py
+++ chainlit_app.py
@@ -1,11 +1,50 @@
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import Runnable
+from langchain.schema.runnable.config import RunnableConfig
+from typing import cast
+
 import chainlit as cl
+import streamlit as st
+
+
+llm_key = st.secrets["general"]["GROQ_LLM_KEY"]
+llm = ChatOpenAI(
+        model="llama3-8b-8192",
+        temperature=0,
+        openai_api_key=llm_key,
+        openai_api_base="https://api.groq.com/openai/v1",
+        streaming=True
+    )
+
+
+@cl.on_chat_start
+async def on_chat_start():
+    model = llm
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You're a very knowledgeable historian who provides accurate and eloquent answers to historical questions.",
+            ),
+            ("human", "{question}"),
+        ]
+    )
+    runnable = prompt | model | StrOutputParser()
+    cl.user_session.set("runnable", runnable)
 
 
 @cl.on_message
-async def main(message: cl.Message):
-    # Your custom logic goes here...
+async def on_message(message: cl.Message):
+    runnable = cast(Runnable, cl.user_session.get("runnable"))  # type: Runnable
 
-    # Send a response back to the user
-    await cl.Message(
-        content=f"Received: {message.content}",
-    ).send()
+    msg = cl.Message(content="")
+
+    async for chunk in runnable.astream(
+        {"question": message.content},
+        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+    ):
+        await msg.stream_token(chunk)
+
+    await msg.send()
